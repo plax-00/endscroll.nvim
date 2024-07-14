82b20be2a3ec7b8c93cd7b5829903465c2467eb6
plax-00 <plaxman81@gmail.com>
2024-07-13 23:57:11 -0400

    fix: rewrite of scrolling logic to avoid looping and make code cleaner and simpler

--- lua/endscroll/scroll.lua
+++ lua/endscroll/scroll.lua
@@ -1,7 +1,9 @@
-local api = vim.api
-local fn = vim.fn
+local line = vim.fn.line
 local opts = require('endscroll.config').opts
-local scroll_key = vim.keycode('<C-e>')
+
+local function get_text_height(start_line, end_line)
+    return vim.api.nvim_win_text_height(0, { start_row = start_line - 1, end_row = end_line - 1 }).all - 1
+end
 
 local function scroll()
     -- check for disabled filetype
@@ -11,40 +13,25 @@ local function scroll()
     end
 
     local scrolloff = vim.o.scrolloff
-    local last_line = fn.line('$')
-    local current_line = fn.line('.')
-    local top_line = fn.line('w0')
+    local last_line = line('$')
+    local current_line = line('.')
     local count = vim.v.count1
 
+    local lines_to_end = get_text_height(current_line, last_line)
+    local target_line = current_line + count
+    local endscroll_start = current_line + lines_to_end - scrolloff
 
+    vim.cmd.normal { count .. 'j', bang = true  }
 
-    for _ = 1, count, 1 do
-        local above = api.nvim_win_text_height(0, { start_row = top_line - 1, end_row = current_line - 1}).all - 1
-        local below = current_line == last_line and 0 or api.nvim_win_text_height(0, { start_row = current_line - 1, end_row = last_line - 1}).all - 2
+    if target_line > endscroll_start then
+        local lines_above = get_text_height(line('w0'), line('.'))
+        local max_scroll = lines_above - scrolloff
 
-        if current_line == last_line then
-            if above > scrolloff and opts.scroll_at_end then
-                vim.cmd.normal { scroll_key, bang = true }
-                goto continue
-            else
-                return
-            end
-        end
+        if count > max_scroll then count = max_scroll end
+        if count < 1 then return end
 
-        if below < scrolloff and above >= scrolloff then
-            vim.cmd.normal { 'j' .. scroll_key, bang = true }
-            goto continue
-        end
-
-        vim.cmd.normal { 'j', bang = true }
-
-        ::continue::
-        if current_line < last_line - 1 then
-            current_line = current_line + 1
-        end
+        vim.cmd.normal { count .. vim.keycode('<C-e>'), bang = true }
     end
-
 end
-
 
 return scroll
