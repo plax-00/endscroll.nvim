4f9c7ac1d827a205ebea53ced57fc845b6da4194
plax-00 <plaxman81@gmail.com>
2024-07-20 03:35:01 -0400

    feat: work with CTRL-D (#1)

--- lua/endscroll/init.lua
+++ lua/endscroll/init.lua
@@ -5,7 +5,9 @@ M.setup = function(opts)
     config.opts = vim.tbl_deep_extend('force', config.defaults, opts)
 
     local scroll = require('endscroll.scroll')
-    vim.keymap.set({ 'n', 'v' }, 'j', scroll, { silent = true })
+    vim.keymap.set({ 'n', 'v' }, 'j',     function() scroll('j')     end, { silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<C-d>', function() scroll('<C-d>') end, { silent = true })
+
     require('endscroll.autocmds')
 end
 
--- lua/endscroll/scroll.lua
+++ lua/endscroll/scroll.lua
@@ -5,30 +5,39 @@ local function get_text_height(start_line, end_line)
     return vim.api.nvim_win_text_height(0, { start_row = start_line - 1, end_row = end_line - 1 }).all - 1
 end
 
-local function scroll()
+local function scroll(key)
     local count = vim.v.count1
 
     -- check for disabled filetype
     if vim.tbl_contains(opts.disabled_filetypes, vim.o.filetype) then
-        vim.cmd.normal { count .. 'j', bang = true }
+        vim.cmd.normal { count .. vim.keycode(key), bang = true }
         return
     end
 
     local scrolloff = vim.o.scrolloff
     local last_line = line('$')
     local current_line = line('.')
-
     local lines_to_end = get_text_height(current_line, last_line)
-    local target_line = current_line + count
-    local endscroll_start = current_line + lines_to_end - scrolloff
+
+    local ctrl_d = key == '<C-d>'
+    local pre_lines_above
+    if ctrl_d then
+        if count == 1 then
+            count = vim.o.scroll
+        end
+        pre_lines_above = get_text_height(line('w0'), current_line)
+    end
 
     vim.cmd.normal { count .. 'j', bang = true  }
 
-    if target_line > endscroll_start then
+    if count > lines_to_end - scrolloff or ctrl_d then
         local lines_above = get_text_height(line('w0'), line('.'))
         local max_scroll = lines_above - scrolloff
         if not opts.scroll_at_end then
             max_scroll = lines_above - (vim.fn.winheight(0) - scrolloff - 1)
+        end
+        if ctrl_d then
+            max_scroll = lines_above - pre_lines_above
         end
 
         if count > max_scroll then count = max_scroll end
