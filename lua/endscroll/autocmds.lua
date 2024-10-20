dc943ab277a2eb0518868299ca03d6d0710fe01a
plax-00 <plaxman81@gmail.com>
2024-10-19 22:18:48 -0400

    fix: winview should be local to windows not buffers (#3)

--- lua/endscroll/autocmds.lua
+++ lua/endscroll/autocmds.lua
@@ -2,22 +2,22 @@ local api = vim.api
 local fn = vim.fn
 
 local function save_window_view()
-    vim.b.window_view = fn.winsaveview()
+    vim.w.endscroll_window_view = fn.winsaveview()
 end
 
 local function load_window_view()
-    if vim.b.window_view then
-        fn.winrestview(vim.b.window_view)
+    if vim.w.endscroll_window_view then
+        fn.winrestview(vim.w.endscroll_window_view)
     end
 end
 
 api.nvim_create_augroup('EndscrollRestoreWinView', { clear = true })
-api.nvim_create_autocmd('BufLeave', {
+api.nvim_create_autocmd('WinLeave', {
     group = 'EndscrollRestoreWinView',
     pattern = '*',
     callback = save_window_view,
 })
-api.nvim_create_autocmd('BufEnter', {
+api.nvim_create_autocmd('WinEnter', {
     group = 'EndscrollRestoreWinView',
     pattern = '*',
     callback = load_window_view,
